31eaa9a51504cb85a30510b3f4655f2b219487d6
nextonesfaster <nextonesfaster@gmail.com>
2024-09-11 13:43:12 +0530

    separate home text into sections

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { homeData } from '../data/text.js';
 
+interface Section {
+    title: string;
+    text: string;
+}
+
 const Home: React.FC = () => {
+    const [sections, setSections] = useState<Section[]>([]);
+
+    useEffect(() => {
+        setSections(homeData)
+    }, []);
+
     return (
         <div className="container">
             <div className="home-text">
-                {homeData.text.split('\n').map((line, index) => (
-                    <p key={index}>{line}</p>
+                {sections.map((section, sectionIndex) => (
+                    <div key={sectionIndex}>
+                        <h3>{section.title}</h3>
+                        {section.text.split('\n').map((line, index) => (
+                            <p key={index}>{line}</p>
+                        ))}
+                    </div>
                 ))}
             </div>
         </div>

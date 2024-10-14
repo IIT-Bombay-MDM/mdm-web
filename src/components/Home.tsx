1a6766359ce62a368cae28380e7e8f7182768f26
nextonesfaster <nextonesfaster@gmail.com>
2024-10-14 15:54:37 +0530

    add drone images, some team images

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,27 +4,31 @@ import { homeData } from '../data/text.js';
 interface Section {
     title: string;
     text: string;
+    image?: string;
 }
 
 const Home: React.FC = () => {
     const [sections, setSections] = useState<Section[]>([]);
 
     useEffect(() => {
-        setSections(homeData)
+        setSections(homeData);
     }, []);
 
     return (
         <div className="container">
             <div className="home-text">
                 {sections.map((section, sectionIndex) => (
-                    <div key={sectionIndex}>
-                        <h3>{section.title}</h3>
-                        {section.text.split('\n').map((line, index) => (
-                            <p
-                                key={index}
-                                dangerouslySetInnerHTML={{ __html: line }}
-                            />
-                        ))}
+                    <div key={sectionIndex} className="section">
+                        <div className="text-content">
+                            <h3>{section.title}</h3>
+                            {section.text.split('\n').map((line, index) => (
+                                <p
+                                    key={index}
+                                    dangerouslySetInnerHTML={{ __html: line }}
+                                />
+                            ))}
+                        </div>
+                        {section.image && <img src={section.image} alt={section.title} className="section-image" />}
                     </div>
                 ))}
             </div>
